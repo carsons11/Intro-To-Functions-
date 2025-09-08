991a9920600014f9f49391a24c341c78363e14ff
carsons11 <carsons11@nycstudents.net>
2025-09-08 09:24:16 -0400

    added hello world

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("hello")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
+
+turtle.done()

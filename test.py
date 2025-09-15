9d26ae583b9be6ec816d6acf927e1ef855784683
carsons11 <carsons11@nycstudents.net>
2025-09-15 06:25:42 -0700

    Update test.py

--- test.py
+++ test.py
@@ -70,12 +70,58 @@ print(int(bill) * .15) """
 #use F string
 print(f"His name is {name}")
  """
+
+""" #Float
+#bill = 3.14
+students = ["Cadee", "Mason", "Andy"]
+#Can reference each item in a list by their index
+print(students[0]) #Prints Cadee
+
+#add student
+students.append("Alina") #append adds it into the list
+
+#we can iterate or loop through a list
+for student in students:
+    print(student) """
+
+""" #BooLean true or false
+x = True
+y = False
+#for evaluations use double equal signs ==
+if x == True:
+    print("Hello Rodney") 
+else:
+    print("Goodbye Rodney") """
+
+""" students = ["Cadee", "Mason", "Andy", "Alina"]
+if "Alina" in students:
+    print("She's here")
+else:
+    students.append("Alina")
+    print("We added Alina")
+ """
+""" x = 91
+if x <10:
+    print("Less")
+elif x == 10:
+    print("Equal")
+else:
+    print("Greater than 10")
+
+students = ["Cadee", "Mason", "Andy", "Alina"]
+for student in students:
+    found = False
+    if student == "Mason":
+        print("Found Mason")
+        found = True """
+""" name = "Cadee"
+for letter in name:
+    print(letter)"""  
+
 import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-
-
 
 def square(x):
     t.forward(x)
@@ -87,7 +133,15 @@ def square(x):
     t.forward(x)
     t.left(90)
 
-for i in range(100):
-    square(200)
-    t.right(5)
-    t.speed(500) 
+def square():
+    for i in range(60):
+        square(5)
+        t.right(5)
+        t.speed(200)
+def addsquares(x):
+        length = 25
+        for i in range(60):
+            square(length,90)
+            length = length * 5 
+addsquares()
+turtle.done()
